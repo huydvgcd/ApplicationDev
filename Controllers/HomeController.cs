b20f802b733424946103e855f899f2a7280c5f4a
Minh <minhnnqgcd191322@fpt.edu.vn>
2022-08-28 21:51:00 +0700

    test commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace ApplicationDev.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Privacy()ádsadasd
         {
             return View();
         }
